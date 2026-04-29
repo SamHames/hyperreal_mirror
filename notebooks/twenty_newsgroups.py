2183c1d35ae08b45e84d26eb4064c44f40fa83bf
sam_hames <s.hames@uq.edu.au>
2026-04-29 23:54:18 +0000

    Add kernel spec to the pypercent twenty_newsgroups notebook
    FossilOrigin-Name: dda38f6c5cd3417c039a992742205b351cc91097fe85f32ac9f80c9a18900004

--- notebooks/twenty_newsgroups.py
+++ notebooks/twenty_newsgroups.py
@@ -1,3 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.6
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
 # %% [markdown]
 # # Worked Example: The Twenty Newsgroups
 #
